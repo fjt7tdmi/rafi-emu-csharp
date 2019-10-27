4f9a3cdeda5d2d669abb449c62fa67fe83cd7217
Akifumi Fujita <fujita.akifumi@gmail.com>
2019-10-27 12:38:02 +0900

    add default C# project

---
+++ RafiEmu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RafiEmu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
